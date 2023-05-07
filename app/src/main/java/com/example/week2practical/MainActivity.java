9f4f265ae1dd7da124a82831e767ad3a0360fd3c
s10242330 <s10242330@connect.np.edu.sg>
2023-05-07 13:25:07 +0800

    1

--- app/src/main/java/com/example/week2practical/MainActivity.java
+++ app/src/main/java/com/example/week2practical/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.week2practical;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User User1 = new User("John", "Person", 12, false);
+        String Name = User1.getName();
+        String Description = User1.getDescription();
+
+        Button followButton = findViewById(R.id.button);
+        followButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(User1.followed == true){
+                    User1.followed = false;
+                    followButton.setText("Follow");
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_SHORT).show();
+                }
+                else if(User1.followed == false){
+                    User1.followed = true;
+                    followButton.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
